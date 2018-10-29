20cf96c3e1385c4f2a60cf04883879b0c430482d
Andrés Felipe López <af.lopezf@uniandes.edu.co>
2018-10-28 20:10:04 -0500

    Mensajes de error en formulario y detail

--- imports/api/activities.js
+++ imports/api/activities.js
@@ -50,6 +50,9 @@ Meteor.methods({
       $inc: { capacity: -1},
       $push: { participants: Meteor.users.findOne(this.userId).username}
     });
+
+    const answer = Activities.findOne({_id: activityId});
+    return answer;
   },
 
 });
