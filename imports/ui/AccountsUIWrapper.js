0d79440dc84b6a05e535bf29c23ea97e994065f9
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-10-31 17:31:34 -0500

    Reemplazar accounts-ui

--- imports/ui/AccountsUIWrapper.js
+++ imports/ui/AccountsUIWrapper.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
- 
+
+
+//Pueden hacer su propio formulario de registro e inicio de sesion utlizando los metodos de Accounts
+//https://docs.meteor.com/api/passwords.html y https://docs.meteor.com/api/accounts.html
+//de esta forma se puede hacer el registro de un usuario Administrador y agregar todos los campos que quieran
+//en un campo 'profile'
 export default class AccountsUIWrapper extends Component {
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
